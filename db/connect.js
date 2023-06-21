88e6564a9acc53c11ff0e636330f266323b4e58a
ma.navarro <ma.navarro@neoco.dev>
2023-06-21 15:25:42 +0200

    Connection to DDBB with mongoose

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,15 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://admin:<password>@atlascluster.pug0fgz.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin@atlascluster.pug0fgz.mongodb.net/NotebookApp?retryWrites=true&w=majority";
+
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
